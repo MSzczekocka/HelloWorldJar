1af312315f3438b27565f144110e1cc460a73705
Martyna <martyna.szczekocka@gmail.com>
2021-09-20 23:52:55 +0200

    pom - version 1

---
+++ run.bat
@@ -0,0 +1,1 @@
+java -jar HelloWorldJar-0.0.1-SNAPSHOT.jar
--- src/main/java/com/test/HelloWorldJar/controller/HelloWorldController.java
+++ src/main/java/com/test/HelloWorldJar/controller/HelloWorldController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
     @GetMapping("/hello")
