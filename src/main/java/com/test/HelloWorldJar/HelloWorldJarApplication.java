4f9c6bb3e52649d2702157977a7177aac71533ab
Martyna <martyna.szczekocka@gmail.com>
2021-09-21 20:50:15 +0200

    feature opening localhost in browser

--- src/main/java/com/test/HelloWorldJar/HelloWorldJarApplication.java
+++ src/main/java/com/test/HelloWorldJar/HelloWorldJarApplication.java
@@ -1,13 +1,24 @@
 package com.test.HelloWorldJar;
 
+import com.test.HelloWorldJar.browser.BrowserOpener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 
 @SpringBootApplication
 public class HelloWorldJarApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HelloWorldJarApplication.class, args);
+
+		BrowserOpener browserOpener = new BrowserOpener();
+		browserOpener.browse("http://localhost:8080/hello");
+
 	}
 
 }
