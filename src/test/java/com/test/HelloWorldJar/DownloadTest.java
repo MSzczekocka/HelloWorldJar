e2d17165b58088710232863e7c60dd5e940faa97
Martyna <martyna.szczekocka@gmail.com>
2021-09-24 21:19:48 +0200

    app version 1

--- src/test/java/com/test/HelloWorldJar/DownloadTest.java
+++ src/test/java/com/test/HelloWorldJar/DownloadTest.java
@@ -1,19 +1,11 @@
 package com.test.HelloWorldJar;
 
-import com.test.HelloWorldJar.browser.Download;
-import com.test.HelloWorldJar.controller.HelloWorldController;
-import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 
 import java.io.File;
 
-import static junit.framework.TestCase.assertTrue;
 
-@RequiredArgsConstructor
 public class DownloadTest {
 
     @Test
