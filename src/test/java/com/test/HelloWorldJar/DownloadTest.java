20d11c5a03ed47b5548484a229ec401cae51d6fd
Martyna <martyna.szczekocka@gmail.com>
2021-09-27 21:44:38 +0200

    test ver 2

--- src/test/java/com/test/HelloWorldJar/DownloadTest.java
+++ src/test/java/com/test/HelloWorldJar/DownloadTest.java
@@ -1,16 +1,40 @@
 package com.test.HelloWorldJar;
 
+import com.test.HelloWorldJar.browser.Download;
+import com.test.HelloWorldJar.controller.HelloWorldController;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import java.io.File;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class DownloadTest {
+
 
     @Test
     public void fileExist(){
         boolean check = new File("C:\\test\\hello.txt").exists();
         Assertions.assertTrue(check);
+    }
+
+    @Test
+    public void checkfile(){
+        HelloWorldController helloWorldController = mock(HelloWorldController.class);
+        when(helloWorldController.getDownload()).thenReturn(testDownload());
+        Assert.assertEquals(helloWorldController.getDownload(),"Downloaded");
+        boolean check = new File("C:\\test\\hello.txt").exists();
+        Assertions.assertTrue(check);
+    }
+
+    public String testDownload(){
+        Download download = mock(Download.class);
+        download.createTxt();
+        return "Downloaded";
     }
 }
