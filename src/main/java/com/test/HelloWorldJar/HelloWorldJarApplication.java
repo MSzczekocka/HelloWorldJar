2778369817abe47a317462f31081b8e26c06e606
Martyna <martyna.szczekocka@gmail.com>
2021-10-02 10:21:41 +0200

    custom exception handler ver1

--- src/main/java/com/test/HelloWorldJar/HelloWorldJarApplication.java
+++ src/main/java/com/test/HelloWorldJar/HelloWorldJarApplication.java
@@ -3,19 +3,16 @@ package com.test.HelloWorldJar;
 import com.test.HelloWorldJar.browser.BrowserOpener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @SpringBootApplication
 public class HelloWorldJarApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldJarApplication.class, args);
-		BrowserOpener.browse("http://localhost:8080/hello");
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldJarApplication.class, args);
+        BrowserOpener.browse("http://localhost:8080/hello");
 
-		String currentWorkingDir = System.getProperty("user.dir");
-		System.out.println(currentWorkingDir);
-
-	}
-
-	private void argumentNotValidException(){
-	}
+        String currentWorkingDir = System.getProperty("user.dir");
+        System.out.println(currentWorkingDir);
+    }
 }
+
+//Advice controller
--- src/main/java/com/test/HelloWorldJar/browser/BrowserOpener.java
+++ src/main/java/com/test/HelloWorldJar/browser/BrowserOpener.java
@@ -6,33 +6,33 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class BrowserOpener {
-
     public static void browse(String url) {
-//  Desktop - class from package java.awt.
-//    Supported operations include:
-//      launching the user-default browser to show a specified URI;
-//      launching the user-default mail client with an optional mailto URI;
-//      launching a registered application to open, edit or print a specified file.
-//  isDesktopSupported() - tests whether this class is supported on the current platform.
+        /*  Desktop - class from package java.awt.
+            Supported operations include:
+              launching the user-default browser to show a specified URI;
+              launching the user-default mail client with an optional mailto URI;
+              launching a registered application to open, edit or print a specified file.
+                isDesktopSupported() - tests whether this class is supported on the current platform.
+        */
         if (Desktop.isDesktopSupported()) {
             Desktop desktop = Desktop.getDesktop();
-//  browse() - Launches the default browser to display a URI.
+            //  browse() - Launches the default browser to display a URI.
             try {
                 desktop.browse(new URI(url));
-//  IOException - Signals that an I/O exception of some sort has occurred.
-//  URISyntaxException - Checked exception thrown to indicate that a string could not be parsed as a URI reference.
-//  printStackTrace() - prints the throwable along with other details like the line number and class name where the exception occurred.
+                // IOException - Signals that an I/O exception of some sort has occurred.
+                // URISyntaxException - Checked exception thrown to indicate that a string could not be parsed as a URI reference.
+                // printStackTrace() - prints the throwable along with other details like the line number and class name where the exception occurred.
             } catch (IOException | URISyntaxException e) {
                 e.printStackTrace();
             }
         } else {
-//  Runtime - allows the application to interface with the environment in which the application is running.
-//  getRuntime() - returns the runtime object associated with the current Java application.
+            //  Runtime - allows the application to interface with the environment in which the application is running.
+            //  getRuntime() - returns the runtime object associated with the current Java application.
             Runtime runtime = Runtime.getRuntime();
             try {
-//  exec - executes the specified string command in a separate process.
-//  rundll32 url.dll - execute 32-bit DLL file
-//  DLL file -  Microsoft's implementation of the shared library
+                // exec - executes the specified string command in a separate process.
+                // rundll32 url.dll - execute 32-bit DLL file
+                // DLL file -  Microsoft's implementation of the shared library
                 runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
             } catch (IOException e) {
                 e.printStackTrace();
